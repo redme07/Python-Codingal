21fa7a4a7e2a4eb4e24d85b96682a902faf399b8
redme07 <atharv.vaish07@gmail.com>
2025-06-19 16:26:21 +0530

    Lesson 1

---
+++ Lesson 1/print statements and comments.py
@@ -0,0 +1,9 @@
+
+#printing a greeting message
+print("hello, I am Atharv")
+#printing an introduction about me
+print("I am 12 years old, live in banglore and like biology")
+#printing a fun fact about myslef
+print("I like playing cricket and table tennis")
+#performing and printing a simple calculation
+print("The result of 78 + 54 is", 78 +54)
