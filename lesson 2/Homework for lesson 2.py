bf5b15039a9908cb8276fa628836f8d441bc3637
redme07 <atharv.vaish07@gmail.com>
2025-06-24 16:23:35 +0530

    homework

---
+++ lesson 2/Homework for lesson 2.py
@@ -0,0 +1,12 @@
+birthday1 = "7/06/2013"
+birthday2 = "10/03/1984"
+birthday3 = "27/06/1982"
+birthday4 = "17/12/2018"
+birthday5 = "19/12/2012"
+
+print(birthday1)
+print(birthday2)
+print(birthday3)
+print(birthday4)
+print(birthday5)
+
