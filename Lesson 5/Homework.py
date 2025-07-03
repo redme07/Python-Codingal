e0a5268621d840df573925f78c3684c344c06e95
redme07 <atharv.vaish07@gmail.com>
2025-07-03 15:35:07 +0530

    Homework

---
+++ Lesson 5/Homework.py
@@ -0,0 +1,7 @@
+temperature = float(input("Enter the temperature you are currently experiencing"))
+
+if(temperature>15):
+    print("Wear summer clothes")
+
+else:
+    print("Wear winter clothes")
