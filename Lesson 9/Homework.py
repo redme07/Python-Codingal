235dc8e80cbf56532b0e0d3646c3c53c078a965d
redme07 <atharv.vaish07@gmail.com>
2025-07-10 15:31:16 +0530

    Homework

---
+++ Lesson 9/Homework.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age"))
+
+if(age >= 10) and (age <= 20):
+    print("You are eligable to enroll in this class")
+
+else:
+    print("You are not eligable to enroll in the class")
