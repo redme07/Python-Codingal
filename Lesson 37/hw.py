004d11c9fdd956cf087d4eb50308e1d4afed1fa1
redme07 <atharv.vaish07@gmail.com>
2025-11-27 16:26:42 +0530

    hw

--- Lesson 37/hw.py
+++ Lesson 37/hw.py
@@ -1,13 +1,15 @@
 def evod(a,size):
     cmax = 0
     max = 0
-
+    a2 = []
     for i in range(0,size):
-        if i % 2 == 0 and i+1 % 2 == 1:
+        if a[i] % 2 == 0 and a[i+1] % 2 == 1:
             cmax += 1
+            a2.append(a[i])
+            a2.append(a[i+1])
         if max < cmax:
             max = cmax
-    return max
+    return a2
 
 a = [1,2,3,4,5,6,7,8,9]
 
